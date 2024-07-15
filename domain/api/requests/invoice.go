ccc688ddc96aa822dbeeab6066ebe0ce6f8ffd38
Unknowns24 <unknowns0074@gmail.com>
2024-07-15 17:14:54 -0300

    Improved CreateVoucherRequest

--- domain/api/requests/invoice.go
+++ domain/api/requests/invoice.go
@@ -5,22 +5,9 @@ import (
 	"github.com/seuscode/afip-sdk-go/domain/voucher"
 )
 
-/*
- * @param int $DocTipo Tipo de documento del comprador (REQUIRED, tipo 99 y numero de documento 0 para consumidor final, Facturas B y C hasta 95812 ó 191624 pesos, ver $limiteConsumidorFinal) (https://www.afip.gob.ar/fe/emision-autorizacion/datos-comprobantes.asp)
- *
- * //Ver descripcion de $items mas adelante.
- * @param array $Items Items del comprobante (REQUIRED. No Es estandar de AFIP, es propio del API y se usa para calcular otros importes que AFIP necesita, como ImpTotal, ImpNeto, ImpIVA, ImpOpEx, ImpTotConc)
- *
- * @param string $FchServDesde [NO SE USA EN CONCEPTO 1] Fecha de inicio del servicio (formato YYYYMMDD) (REQUIRED)
- * @param string $FchServHasta [NO SE USA EN CONCEPTO 1] Fecha de fin del servicio (formato YYYYMMDD) (REQUIRED)
- * @param string $FchVtoPago [NO SE USA EN CONCEPTO 1] Fecha de vencimiento del pago (formato YYYYMMDD)	(REQUIRED)
- *
- * @param array $Opcionales [OPCIONAL] Opcionales (Puede no enviarse, o enviarse null. Se usa para informacion complementaria/adicional en el comprobante)
- * @param array $Compradores [OPCIONAL] Compradores (Puede no enviarse, o enviarse null. Se usa para informacion de los compradores en caso de que haya mas de uno)
- */
 type (
 	CreateVoucher struct {
-		PtoVta string `json:"PtoVta"`
+		PtoVta int `json:"PtoVta"`
 
 		CbteTipo voucher.VoucherType    `json:"CbteTipo"`
 		Concepto voucher.VoucherConcept `json:"Concepto"`

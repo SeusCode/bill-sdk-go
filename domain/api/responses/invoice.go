117afa379a7fac61d0a0fb7fb43a1dbaa576b8e4
Unknowns24 <unknowns0074@gmail.com>
2024-07-18 18:24:47 -0300

    Added new fields sent by the API

--- domain/api/responses/invoice.go
+++ domain/api/responses/invoice.go
@@ -2,8 +2,11 @@ package responses
 
 type (
 	CreateInvoiceResponse struct {
-		CaeNumber         int `json:"CAE"`
-		VoucherNumber     int `json:"voucher_number"`
-		CaeExpirationDate int `json:"CAEFchVto"`
+		CaeNumber         string      `json:"CAE"`
+		VoucherNumber     int         `json:"voucher_number"`
+		CaeExpirationDate string      `json:"CAEFchVto"`
+		AfipInfo          interface{} `json:"info_to_afip"`
+		QrString          string      `json:"string_qr_b64"`
+		QrPresent         bool        `json:"qr_present"`
 	}
 )

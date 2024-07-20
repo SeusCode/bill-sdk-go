ee7773e3d215af84cf92f4531ae70cba49c90b81
Unknowns24 <unknowns0074@gmail.com>
2024-07-20 11:15:50 -0300

    Typed afip info returned by API

--- models/afip/voucher/response.go
+++ models/afip/voucher/response.go
@@ -1,13 +1,32 @@
 package voucher
 
+import "github.com/seuscode/bill-sdk-go/models/afip/document"
+
 type (
+	AfipInformation struct {
+		VoucherDate  string                `json:"CbteFch"`
+		VoucherType  VoucherType           `json:"CbteTipo"`
+		Concept      VoucherConcept        `json:"Concepto"`
+		Document     int64                 `json:"DocNro"`
+		DocumentType document.DocumentType `json:"DocTipo"`
+		ImpIVA       float64               `json:"ImpIVA"`
+		ImpNeto      float64               `json:"ImpNeto"`
+		ImpOpEx      float64               `json:"ImpOpEx"`
+		ImpTotal     float64               `json:"ImpTotal"`
+		ImpTotConc   float64               `json:"ImpTotConc"`
+		ImpTrib      float64               `json:"ImpTrib"`
+		MonCotiz     float64               `json:"MonCotiz"`
+		MonId        string                `json:"MonId"`
+		PointOfSale  int                   `json:"PtoVta"`
+		Iva          []VoucherShare        `json:"Iva"`
+	}
+
 	CreateVoucherResponse struct {
-		CaeNumber         string      `json:"CAE"`
-		VoucherNumber     int         `json:"voucher_number"`
-		CaeExpirationDate string      `json:"CAEFchVto"`
-		AfipInfo          interface{} `json:"info_to_afip"`
-		QrString          string      `json:"string_qr_b64"`
-		QrPresent         bool        `json:"qr_present"`
+		CaeNumber         string          `json:"CAE"`
+		VoucherNumber     int             `json:"voucher_number"`
+		CaeExpirationDate string          `json:"CAEFchVto"`
+		AfipInfo          AfipInformation `json:"info_to_afip"`
+		QrEndpoint        string          `json:"qr_endpoint"`
 	}
 
 	GetVoucherTypesResponse struct {

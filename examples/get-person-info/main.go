115b66c3fdebfc2b68fe9b4791bad60d22506062
Unknowns24 <unknowns0074@gmail.com>
2024-07-20 07:38:09 -0300

    Added new example to get person information by taxId or id

---
+++ examples/get-person-info/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/seuscode/bill-sdk-go/api/afip"
+	"github.com/seuscode/bill-sdk-go/models/afip/document"
+	"github.com/seuscode/bill-sdk-go/models/afip/fiscal"
+	"github.com/seuscode/bill-sdk-go/models/api"
+)
+
+func main() {
+	/*
+	 To execute this test you have to replace the following path
+	 for a path to your certificate and private key or create the
+	 certs folder in the root of this project and put them inside.
+	*/
+	certificate, err := os.ReadFile("./certs/web.crt")
+	if err != nil {
+		panic(fmt.Errorf("could not read the certificate %w", err))
+	}
+
+	privateKey, err := os.ReadFile("./certs/web.key")
+	if err != nil {
+		panic(fmt.Errorf("could not read the private key %w", err))
+	}
+
+	afip, err := afip.NewAfipManager(afip.AfipOptions{
+		Certificate: certificate,
+		PrivateKey:  privateKey,
+
+		// Replace this information with yours
+		TaxId:           20123456780,
+		BusinessName:    "YOUR BUSINESS NAME",
+		FiscalAddress:   "YOUR FISCAL ADDRESS",
+		FiscalType:      fiscal.IVA_RESPONSABLE_INSCRIPTO,
+		StartOfActivity: "YOUR START OF ACTIVITY DATE", // (dd/mm/yyyy)
+		PointOfSale:     1,
+		Enviroment:      api.PRODUCTION,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = afip.GetAuthToken()
+	if err != nil {
+		panic(fmt.Errorf("error happend on GetAuthToken %w", err))
+	}
+
+	personIDs := []int{
+		// Add here all the taxId of person you want to get the info
+	}
+
+	for _, personID := range personIDs {
+		res, err := afip.Registry.GetPersonInformation(13, int64(personID), document.CUIT)
+		if err != nil {
+			log.Fatalf("Error getting information for personID %d: %v", personID, err)
+		}
+
+		resp, err := json.Marshal(res)
+		if err != nil {
+			log.Fatalf("Error marshaling response for personID %d: %v", personID, err)
+		}
+
+		fmt.Println(string(resp), "\n ")
+	}
+}

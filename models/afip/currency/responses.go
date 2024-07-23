c1692fed08eae9c01eb187b54036cd6685d279cb
Unknowns24 <unknowns0074@gmail.com>
2024-07-23 02:22:18 -0500

    Added new structures to type API response

--- models/afip/currency/responses.go
+++ models/afip/currency/responses.go
@@ -1,5 +1,11 @@
 package currency
 
-type GetCurrencyTypesResponse struct {
-	Currencies []Currency `json:"currencies_types"`
-}
+type (
+	GetCurrencyTypesResponse struct {
+		Currencies []Currency `json:"currencies_types"`
+	}
+
+	GetCurrencyCotizationResponse struct {
+		CotizationData Cotization `json:"monCotiz"`
+	}
+)
--- models/afip/currency/types.go
+++ models/afip/currency/types.go
@@ -7,4 +7,14 @@ type (
 		StartDate string `json:"FchDesde"`
 		EndDate   string `json:"FchHasta"`
 	}
+
+	Cotization struct {
+		CotizationDate  string  `json:"FchCotiz"`
+		CotizationPrice float64 `json:"MonCotiz"`
+		CurrencyId      string  `json:"MonId"`
+		Description     string  `json:"description"`
+		Full            string  `json:"full"`
+		Short           string  `json:"short"`
+		Timestamp       int64   `json:"timestamp"`
+	}
 )

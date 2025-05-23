11bc5f951235cd2883cfb477992a9a20b6908765
Unknowns24 <unknowns0074@gmail.com>
2025-05-23 02:41:12 -0300

    Added language types

--- models/api/main.go
+++ models/api/main.go
@@ -1,9 +1,14 @@
 package api
 
 type Enviroment uint
+type Language string
 
 const (
 	// Enviroment Types
 	TESTING    Enviroment = 1
 	PRODUCTION Enviroment = 0
+
+	// Language Types
+	SPANISH Language = "es"
+	ENGLISH Language = "en"
 )

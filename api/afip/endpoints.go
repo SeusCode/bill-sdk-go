a1363b68ad4b09b077195a69f7b4fb4f97004dc2
Unknowns24 <unknowns0074@gmail.com>
2025-05-23 02:42:10 -0300

    Moved endpoints from pkg folder to arca client adapter

---
+++ api/afip/endpoints.go
@@ -0,0 +1,19 @@
+package afip
+
+const (
+	ENDPOINT_PING                   = "/ping"
+	ENDPOINT_AFIP_STATUS            = "/status"
+	ENDPOINT_INVOICE                = "/invoice"
+	ENDPOINT_INVOICE_PDF            = "/invoice/pdf"
+	ENDPOINT_POINTS_OF_SALES        = "/pos"
+	ENDPOINT_TRIBUTES               = "/tributes"
+	ENDPOINT_CONCEPTS               = "/concepts"
+	ENDPOINT_ALIQUOTS               = "/aliquots"
+	ENDPOINT_VOUCHERS               = "/vouchers"
+	ENDPOINT_OPTIONALS              = "/optionals"
+	ENDPOINT_DOCUMENTS              = "/documents"
+	ENDPOINT_CURRENCIES             = "/currencies"
+	ENDPOINT_CURRENCY_EXCHANGE_RATE = "/currency/{currencyId}/exchange-rate"
+	ENDPOINT_PAYMENT_METHODS        = "/payment-methods"
+	ENDPOINT_CITIZEN_DATA           = "/p{padronId}/citizen"
+)
